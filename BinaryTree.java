8db60fc6c91b290780a66a821c711beb89e79d8e
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-15 12:03:09 +0530

    Added: Binary Tree Programme

---
+++ BinaryTree.java
@@ -0,0 +1,45 @@
+package com.BinaryTree;
+
+public class BinaryTree<K extends Comparable<K>> {
+
+    private Node<K> root;
+
+    private Node<K> addRecursively(Node<K> current, K key) {
+
+        if (current == null) {
+            return new Node<K>(key);
+        }
+        int compareResult = key.compareTo(current.key);
+
+        if (compareResult == 0)
+            return current;
+        if (compareResult < 0)
+            current.left = addRecursively(current.left, key);
+        else
+            current.right = addRecursively(current.right, key);
+
+        return current;
+
+    }
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    private int getSizeRecursively(Node<K> current) {
+        return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+    }
+
+    public int getSize() {
+        return this.getSizeRecursively(root);
+    }
+
+    public void printBinaryTree() {
+        System.out.println("My Tree: " + root);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBinaryTree{" + root + '}';
+    }
+}
---
+++ BinaryTreeMain.java
@@ -0,0 +1,15 @@
+package com.BinaryTree;
+
+public class BinaryTreeMain {
+    public static void main(String[] args) {
+
+        System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
+
+        BinaryTree<Integer> myBinaryTree = new BinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.printBinaryTree();
+    }
+}
